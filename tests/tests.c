628015faefe8e688f77d7c522024ccdeb1fad2b7
kensj <kenji.japra@gmail.com>
2018-09-28 14:09:57 -0400

    project setup

---
+++ tests/tests.c
@@ -0,0 +1,11 @@
+#include <criterion/criterion.h>
+/*#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>*/
+
+/*Test(sf_memsuite, Init, .init = func, .fini = func) {
+  cr_assert(comparison, "Fail");
+}
+*/
