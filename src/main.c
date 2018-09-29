1f7e63d0fb3509bda26216e6076da662bc9c212f
kensj <kenji.japra@gmail.com>
2018-09-29 04:04:50 -0400

    init main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
 /*#include <stdio.h>*/
 #include <stdlib.h>
+#include <string.h>
+#include "singly-linked-list.h"
 
 int main(int argc, char *argv[]) {
-
+	/*sll_insert(10);
+	sll_insert(20);
+	sll_insert(30);
+	sll_print();
+	sll_free();*/
     return EXIT_SUCCESS;
 }
