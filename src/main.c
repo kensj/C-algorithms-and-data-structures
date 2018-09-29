cfe0f90fc678397e096ff77771256882ec116e56
kensj <kenji.japra@gmail.com>
2018-09-29 15:38:48 -0400

    cleanup main file

--- src/main.c
+++ src/main.c
@@ -4,12 +4,5 @@
 #include "singly-linked-list.h"
 
 int main(int argc, char *argv[]) {
-	sll_insert(10);
-	sll_insert(20);
-	sll_insert(30);
-	sll_print();
-	sll_poll_tail();
-	sll_print();
-	sll_free();
     return EXIT_SUCCESS;
 }
