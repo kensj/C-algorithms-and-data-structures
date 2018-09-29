afc29c04591292868f86a661a5d0affb64f5b7bd
kensj <kenji.japra@gmail.com>
2018-09-29 04:04:20 -0400

    singly linked list: added tests

--- tests/tests.c
+++ tests/tests.c
@@ -1,11 +1,25 @@
 #include <criterion/criterion.h>
+#include "singly-linked-list.h"
 /*#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>*/
 
-/*Test(sf_memsuite, Init, .init = func, .fini = func) {
-  cr_assert(comparison, "Fail");
+Test(sf_memsuite, First_value, .fini = sll_free) {
+	Node* node = sll_insert(10);
+  	cr_assert(node->value == 10, "Value not equal!");
+  	cr_assert(node->next == NULL, "List size greater than 1!");
 }
-*/
+
+Test(sf_memsuite, Three_values, .fini = sll_free) {
+	Node* node3 = sll_insert(10);
+	Node* node2 = sll_insert(20);
+	Node* node1 = sll_insert(30);
+  	cr_assert(node1->value == 30, "First value incorrect!");
+  	cr_assert(node1->next == node2, "First's next element is not the second!");
+  	cr_assert(node2->value == 20, "Second value incorrect!");
+  	cr_assert(node2->next == node3, "Second's next element is not the third!");
+  	cr_assert(node3->value == 10, "Third value incorrect!");
+  	cr_assert(node3->next == NULL, "Third's next element is not null!");
+}
