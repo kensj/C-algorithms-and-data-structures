243bec0ca4e4d543a75662fd25bae3322352c699
kensj <kenji.japra@gmail.com>
2018-09-29 18:05:23 -0400

    comment

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /*#include <stdio.h>*/
 #include <stdlib.h>
 #include <string.h>
-#include "singly-linked-list.h"
+//#include "singly-linked-list.h"
 
 int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
